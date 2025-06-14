645767a6b2b9a6133586fc0d98abc22c0c77e344
johnmaxrin <johnmaxrin@gmail.com>
2025-06-14 23:42:21 +0530

    Add CL file parse

--- main.cc
+++ main.cc
@@ -3,7 +3,9 @@
 #include "mlir/IR/Operation.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
-
+#include "mlir/Support/FileUtilities.h"
+#include "llvm/Support/SourceMgr.h"
+#include "mlir/Parser/Parser.h"
 
 #include <iostream>
 
@@ -14,10 +16,10 @@
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 #include "conversions/avialirtompi.h"
 // #include "mlir/Conversion/MPIToLLVM/MPIToLLVM.h"
-
 
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
@@ -28,22 +30,55 @@
 #include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
 #include "mlir/Conversion/OpenMPToLLVM/ConvertOpenMPToLLVM.h"
 
-
-
 using namespace std;
 using namespace mlir;
 
-int main()
+int main(int argc, char *argv[])
 {
+    mlir::DialectRegistry registry;
+    registry.insert<mlir::func::FuncDialect>();
+    registry.insert<mlir::memref::MemRefDialect>();
+    registry.insert<mlir::scf::SCFDialect>();
+
     MLIRContext context;
-    OwningOpRef<ModuleOp> module = ModuleOp::create(UnknownLoc::get(&context));
+    context.allowUnregisteredDialects();
+
+    context.appendDialectRegistry(registry);
+
+
+    llvm::cl::opt<std::string> inpFileName(llvm::cl::Positional, llvm::cl::desc("<MLIR INP File>"), llvm::cl::Required);
+    llvm::cl::ParseCommandLineOptions(argc, argv, "CL Parser\n");
+
+    std::string errorMsg;
+    auto mlirFile = mlir::openInputFile(inpFileName, &errorMsg);
+
+    if (!mlirFile)
+    {
+        llvm::errs() << "Failed to open file: " << errorMsg << "\n";
+        return 1;
+    }
+
+    llvm::SourceMgr sourceMgr;
+
+    sourceMgr.AddNewSourceBuffer(std::move(mlirFile), llvm::SMLoc());
+
+    // OwningOpRef<ModuleOp> module = ModuleOp::create(UnknownLoc::get(&context));
+    OwningOpRef<ModuleOp> module = mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &context);
+
+    if (!module) {
+    llvm::errs() << "Failed to parse the MLIR file.\n";
+    return 2;
+  }
+    
+
+    /*
     OpBuilder builder(&context);
 
     context.getOrLoadDialect<avial::AvialDialect>();
     context.getOrLoadDialect<memref::MemRefDialect>();
     context.getOrLoadDialect<scf::SCFDialect>();
     context.getOrLoadDialect<arith::ArithDialect>();
-    
+
 
 
     // Generate a Sample Avial IR
@@ -61,11 +96,11 @@ int main()
     DictionaryAttr arg3 = builder.getDictionaryAttr({builder.getNamedAttr("name", builder.getStringAttr("inp3")),
                                                      builder.getNamedAttr("type", TypeAttr::get(memrefType))});
     ArrayAttr insAttr = builder.getArrayAttr({arg1, arg2, arg3});
-    auto schOp = builder.create<avial::ScheduleOp>(builder.getUnknownLoc(),insAttr, 
+    auto schOp = builder.create<avial::ScheduleOp>(builder.getUnknownLoc(),insAttr,
         [&](mlir::OpBuilder &builder, mlir::Location loc, mlir::Value value, mlir::ValueRange args)
         {
             auto cpu = builder.create<avial::TargetOp>(builder.getUnknownLoc(), avial::TargetRefType::get(builder.getContext(), "CPU"), "CPU", "0");
-            auto t1 = builder.create<avial::TaskOp>(builder.getUnknownLoc(),avial::TaskRefType::get(builder.getContext()),cpu.getResult(), mlir::ValueRange{args[0], args[1] }, mlir::ValueRange{args[2]}, 
+            auto t1 = builder.create<avial::TaskOp>(builder.getUnknownLoc(),avial::TaskRefType::get(builder.getContext()),cpu.getResult(), mlir::ValueRange{args[0], args[1] }, mlir::ValueRange{args[2]},
                 [&](mlir::OpBuilder &builder, mlir::Location loc, mlir::Value value, mlir::ValueRange taskargs){
                     // Create a parallel for all from 0 to 20 and add everything.
                     auto constZero = builder.create<arith::ConstantOp>(loc, builder.getI64Type(), builder.getI64IntegerAttr(0));
@@ -78,9 +113,9 @@ int main()
                             auto val2 = builder.create<memref::LoadOp>(nestedLoc, builder.getI64Type(), iterArgs[1], mlir::ValueRange{indexIv}, builder.getBoolAttr(0));
 
                             auto addVal = builder.create<arith::AddIOp>(nestedLoc, builder.getI64Type(), val1, val2);
-                            
+
                             builder.create<memref::StoreOp>(nestedLoc, addVal.getResult(), iterArgs[2], mlir::ValueRange{indexIv}, builder.getBoolAttr(0));
-                            builder.create<mlir::scf::YieldOp>(loc, mlir::ValueRange{iterArgs}); 
+                            builder.create<mlir::scf::YieldOp>(loc, mlir::ValueRange{iterArgs});
                         });
 
                     builder.create<mlir::avial::YieldOp>(loc);
@@ -89,8 +124,8 @@ int main()
             t1->setAttr("name", builder.getStringAttr("task1"));
             auto inp2 = builder.create<memref::AllocaOp>(loc,memrefType);
             auto inp3 = builder.create<memref::AllocaOp>(loc,memrefType);
-        
-            auto t2 = builder.create<avial::TaskOp>(builder.getUnknownLoc(),avial::TaskRefType::get(builder.getContext()),cpu.getResult(), mlir::ValueRange{args[0], inp3->getResult(0) }, mlir::ValueRange{inp2}, 
+
+            auto t2 = builder.create<avial::TaskOp>(builder.getUnknownLoc(),avial::TaskRefType::get(builder.getContext()),cpu.getResult(), mlir::ValueRange{args[0], inp3->getResult(0) }, mlir::ValueRange{inp2},
                 [&](mlir::OpBuilder &builder, mlir::Location loc, mlir::Value value, mlir::ValueRange taskargs){
                     // Create a parallel for all from 0 to 20 and add everything.
                     auto constZero = builder.create<arith::ConstantOp>(loc, builder.getI64Type(), builder.getI64IntegerAttr(0));
@@ -103,9 +138,9 @@ int main()
                             auto val2 = builder.create<memref::LoadOp>(nestedLoc, builder.getI64Type(), iterArgs[1], mlir::ValueRange{indexIv}, builder.getBoolAttr(0));
 
                             auto addVal = builder.create<arith::AddIOp>(nestedLoc, builder.getI64Type(), val1, val2);
-                            
+
                             builder.create<memref::StoreOp>(nestedLoc, addVal.getResult(), iterArgs[2], mlir::ValueRange{indexIv}, builder.getBoolAttr(0));
-                            builder.create<mlir::scf::YieldOp>(loc, mlir::ValueRange{iterArgs}); 
+                            builder.create<mlir::scf::YieldOp>(loc, mlir::ValueRange{iterArgs});
                         });
 
                     builder.create<mlir::avial::YieldOp>(loc);
@@ -120,9 +155,9 @@ int main()
     // End of generating a Sample Avial IR
     DialectRegistry registry;
     // mpi::registerConvertMPIToLLVMInterface(registry);
-  
+
     context.appendDialectRegistry(registry);
-    
+
 
     // Lowerings.
     PassManager pm(&context);
@@ -133,31 +168,31 @@ int main()
         return 1;
     }
 
-    
+
 
 
     module->dump();
 
     // // Lower Everything to LLVM
     PassManager pm2(&context);
-    
+
     pm2.addPass(createConvertFuncToLLVMPass());
     pm2.addPass(createArithToLLVMConversionPass());
     pm2.addPass(createConvertSCFToOpenMPPass());
     pm2.addPass(createSCFToControlFlowPass());
     pm2.addPass(createConvertControlFlowToLLVMPass());
     pm2.addPass(createConvertOpenMPToLLVMPass());
-    //pm2.addPass(createLowerMPIPass());  
+    //pm2.addPass(createLowerMPIPass());
     pm2.addPass(createFinalizeMemRefToLLVMConversionPass());
 
     if (failed(pm2.run(module->getOperation()))) {
         llvm::errs() << "Failed to run 2 passes\n";
         return 1;
     }
-    
-    
+
+    */
+
     module->dump();
-    
-    
+
     return 0;
 }
