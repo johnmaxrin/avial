8e8129def5de94dd5aad0ddb7abb8918003bc7b6
johnmaxrin <johnmaxrin@gmail.com>
2025-06-17 17:31:32 +0530

    Add Func type jugaad

--- conversions/stdtoavial.h
+++ conversions/stdtoavial.h
@@ -11,6 +11,10 @@
 
 #include "llvm/Support/Casting.h"
 
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+
+#include <string>
+
 using namespace mlir;
 using namespace avial;
 
@@ -29,11 +33,44 @@ namespace mlir
                 mlir::MLIRContext *context = &getContext();
                 auto *module = getOperation();
 
-                llvm::outs() <<"Hello\n";
+                module->walk<mlir::WalkOrder::PreOrder>([&](mlir::Operation *op)
+                {
+                    if (mlir::isa<mlir::func::FuncOp>(op))
+                    {
 
+                        auto funcOp = llvm::dyn_cast<mlir::func::FuncOp>(op);
+
+                        if (auto attr = funcOp->getAttrOfType<mlir::StringAttr>("sym_name"))
+                        {
+                            if(attr.getValue().str().find("cpu") != std::string::npos)
+                            {
+                                llvm::outs() << "Found a CPU Task\n";
+                            }
+                            
+                            else if(attr.getValue().str().find("gpu") != std::string::npos)
+                            {
+                                llvm::outs() << "Found a GPU Task\n";
+                            }
+
+                            else 
+                            {
+                                llvm::outs() << "Unknown Type of Task\n";
+                            }
+                            
+                        }
+                            // op->dumpPretty();
+
+                            // Take Every Functions.
+                            // Check if we have CPU_ or GPU_
+                            // If we don;t have, emit the same IR.
+                            // If we have any functions like that, distribute it!
+                        }
+                    });
+                
+
+            
             }
-        };
+            };
+        }
 
     }
-
-}
